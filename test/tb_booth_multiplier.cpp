38bede3a8536a09c74f7d789a8a8af8ebb565c7f
Zawaher-Bin-Asim <143878349+Zawaher-Bin-Asim@users.noreply.github.com>
2024-01-27 23:16:16 +0500

    Removes load_pp and update coding style

--- test/tb_booth_multiplier.cpp
+++ test/tb_booth_multiplier.cpp
@@ -24,59 +24,59 @@ int a,b;
     top->trace(tfp, 99);  // Trace 99 levels of hierarchy
     tfp->open("waveform.vcd");
 
-	a = rand() ;
+    a = rand() ;
     b = rand() ;
 
 	
     // Simulate for MAX_SIM_TIME
     while (!Verilated::gotFinish() && sim_time < MAX_SIM_TIME) {
         top->reset = 0;
-		if(sim_time >= 2 &&  sim_time < 4){
+	if(sim_time >= 2 &&  sim_time < 4){
 			top->reset = 1;
 			top->in_a = 0;
 			top->in_b = 0;
 			top->valid_in= 0;
-		}
-		top->clk ^= 1;
+	}
+	top->clk ^= 1;
         top->eval();
 
         if(sim_time >= 4 &&  sim_time < 5){
 			top->in_a = a;
 			top->in_b = b;
-		}
+	}
 
-		if(sim_time >= 6 &&  sim_time < 8){
+	if(sim_time >= 6 &&  sim_time < 8){
 
 			top->valid_in = 1;
 		
-		}
-			if(sim_time >= 8 &&  sim_time < 10){
+	}
+	if(sim_time >= 8 &&  sim_time < 10){
 
 			top->valid_in = 0;
 		
-		}
+	}
 
 
-		tfp->dump(sim_time);
-		// Advance time
+	tfp->dump(sim_time);
+	// Advance time
         sim_time++;
      
     }
 	
-	if ((a)*(b) == (top->product)) {
+if ((a)*(b) == (top->product)) {
 		printf("Multiplican =  %d; Multiplier =  %d; Product = %d\n" , a,b, top->product);
 		printf("SIMULATE SUCCESSFULLY\n");
-	}
+}
 
 
 
 
     // Close trace file
-    tfp->close();
+tfp->close();
 
-    // Clean up
-    delete top;
+ // Clean up
+ delete top;
 
-   return 0;
+ return 0;
 }
 
