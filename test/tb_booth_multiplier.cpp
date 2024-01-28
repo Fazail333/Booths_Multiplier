6287c3a356898435048c3e844c5707b801700cc8
Zawaher-Bin-Asim <143878349+Zawaher-Bin-Asim@users.noreply.github.com>
2024-01-29 01:00:07 +0500

    Generalized the test bench  for random inputs

--- test/tb_booth_multiplier.cpp
+++ test/tb_booth_multiplier.cpp
@@ -5,14 +5,17 @@
 #include <verilated_vcd_c.h>
 #include "Vbooth_multiplier.h"  // Replace with the actual Verilated header file
 
-#define MAX_SIM_TIME 43
+#define MAX_SIM_TIME_R 43
+vluint64_t MAX_SIM_TIME = 0;
 vluint64_t sim_time = 0;
 
 int main(int argc, char** argv) {
-short int a,b;
-int product;
+    short int a, b;
+    int product, i;
+    const int array_size = 5;
+    int relative_time[array_size] = {0, 44, 87, 130, 173};
 
-// Verilator setup
+    // Verilator setup
     Verilated::commandArgs(argc, argv);
     Verilated::traceEverOn(true);
 
@@ -24,58 +27,52 @@ int product;
     top->trace(tfp, 99);  // Trace 99 levels of hierarchy
     tfp->open("waveform.vcd");
 
-    a = rand() ;
-    b = rand() ;
+    for (i = 0; i < array_size; i++) {
+        a = rand();
+        b = rand();
+        MAX_SIM_TIME += MAX_SIM_TIME_R;
 
-	
-    // Simulate for MAX_SIM_TIME
-    while (!Verilated::gotFinish() && sim_time < MAX_SIM_TIME) {
-        top->reset = 0;
-	if(sim_time >= 2 &&  sim_time < 4){
-			top->reset = 1;
-			top->in_a = 0;
-			top->in_b = 0;
-			top->valid_in= 0;
-			top->product = 0;
-		}
-	top->clk ^= 1;
-        top->eval();
+        // Simulate for MAX_SIM_TIME
+        while (!Verilated::gotFinish() && sim_time < MAX_SIM_TIME) {
+            top->reset = 0;
+            if ((sim_time >= relative_time[i] + 2) && (sim_time < relative_time[i] + 4)) {
+                top->reset = 1;
+                top->in_a = 0;
+                top->in_b = 0;
+                top->valid_in = 0;
+                top->product = 0;
+            }
+            top->clk ^= 1;
+            top->eval();
 
-        if(sim_time >= 4 &&  sim_time < 5){
-			top->in_a = a;
-			top->in_b = b;
-		}
+            if ((sim_time >= relative_time[i] + 4) && (sim_time < relative_time[i] + 5)) {
+                top->in_a = a;
+                top->in_b = b;
+            }
 
-	if(sim_time >= 6 &&  sim_time < 8){
+            if ((sim_time >= relative_time[i] + 6) && (sim_time < relative_time[i] + 8)) {
+                top->valid_in = 1;
+            }
+            if ((sim_time >= relative_time[i] + 8) && (sim_time < relative_time[i] + 10)) {
+                top->valid_in = 0;
+            }
 
-			top->valid_in = 1;
-		
-		}
-	if(sim_time >= 8 &&  sim_time < 10){
+            tfp->dump(sim_time);
+            product = top->product;
+            // Advance time
+            sim_time++;
+        }
 
-			top->valid_in = 0;
-		
-		}
-
-
-	tfp->dump(sim_time);
-	product = top->product;
-	// Advance time
-	sim_time++;
-     
-    }
-
-	if ((a)*(b) == (product)) {
-        printf("Multiplican =  %d; Multiplier =  %d; Product = %d\n", a, b, product);
-        printf("SIMULATE SUCCESSFULLY\n");
-    }
-	
-	// Close trace file
+        if ((a)*(b) == (product)) {
+            printf("Multiplican =  %d; Multiplier =  %d; Product = %d\n", a, b, product);
+            printf("SIMULATE SUCCESSFULLY\n");
+        }
+    }  // Close trace file
     tfp->close();
 
     // Clean up
     delete top;
 
-   exit(EXIT_SUCCESS);
+    exit(EXIT_SUCCESS);
 }
 
